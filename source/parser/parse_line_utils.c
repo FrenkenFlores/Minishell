b4564badfb717c27b4d32d74e88b2ba263aa96b8
FrenkenFlores <fflores@student.21-school.ru>
2021-01-25 13:28:53 +0300

    update #8: fixed parser error

--- source/parser/parse_line_utils.c
+++ source/parser/parse_line_utils.c
@@ -19,10 +19,10 @@ t_token		*token_init(t_shell *shell, size_t len)
 {
 	t_token		*token;
 
-	if (!(token = ft_calloc(1, sizeof(t_token))) ||		// <------ leak
+	if (!(token = ft_calloc(1, sizeof(t_token))) ||
 		!(token->data = ft_calloc(len + 1, sizeof(char))))
 	{
-		free(token);								// <------
+		free(token);
 		exit_shell(shell, EXIT_FAILURE);
 	}
 	return (token);
--- source/parser/parse_line_utils2.c
+++ source/parser/parse_line_utils2.c
@@ -22,14 +22,20 @@ int		process_double_quote(t_quotes *quote, t_token *token,
 int		process_operators(t_shell *shell, t_token **token,
 										char **line, int i)
 {
+	int j;
+
+	j = 0;
 	if (i)
 	{
 		(*token)->data[i] = '\0';
 		i = 0;
-		(*token)->next = token_init(shell, 1);
+		(*token)->next = token_init(shell, ft_strlen(*line) + 1);
 		*token = (*token)->next;
 	}
-	(*token)->data[0] = *(*line)++;
+	while (*(*line) != '\0' && j < 2)
+		(*token)->data[j++] = *(*line)++;
+	if (j == 2)
+		(*token)->data[j] = '\0';
 	if (**line)
 		*token = create_next_token(shell, *token, *line);
 	return (i);
